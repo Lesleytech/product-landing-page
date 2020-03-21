8e121a5d3a326897eeb61d0f97cc4301bc064e6c
Lafen Lesley <lesleytech6@gmail.com>
2020-03-21 01:55:28 +0000

    File updated

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,6 +1,6 @@
 window.onscroll = function() {
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-    document.getElementById("header").style.background = "#000";
+    document.getElementById("header").style.background = "rgba(0, 0, 0, 0.95)";
   } else {
     document.getElementById("header").style.background = "transparent";
   }
