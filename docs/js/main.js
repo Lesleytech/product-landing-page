8f3a279577adc7c95526e304095c3cb88441a455
Lafen Lesley <lesleytech6@gmail.com>
2020-07-07 16:40:48 +0300

    minor fixes

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,6 +1,5 @@
-ScrollReveal().reveal('.intro', { delay: 250, distance: '50px' });
-ScrollReveal().reveal('#basketballer', {
-  delay: 500,
-  distance: '50px',
-  origin: 'left',
-});
+AOS.init({ duration: 700 });
+
+function handleNavigate(e) {
+  document.getElementById(e).scrollIntoView({ behavior: 'smooth' });
+}
