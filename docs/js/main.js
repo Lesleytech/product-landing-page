40ffb06e930896feb829c6bd187a32dc218ad6d5
Lafen Lesley <lesleytech6@gmail.com>
2020-07-07 13:32:35 +0300

    scrollreveal added

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,7 +1,6 @@
-window.onscroll = function() {
-  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-    document.getElementById("header").style.background = "rgba(0, 0, 0, 0.95)";
-  } else {
-    document.getElementById("header").style.background = "transparent";
-  }
-};
+ScrollReveal().reveal('.intro', { delay: 250, distance: '50px' });
+ScrollReveal().reveal('#basketballer', {
+  delay: 500,
+  distance: '50px',
+  origin: 'left',
+});
